2433bd93a5b41afaf262fe4576db74e5c62efd73
AnotherUser9898 <anotheruser9898@gmail.com>
2026-06-27 22:24:28 +0530

    feat: add improved logging during server startup

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { AppModule } from './app.module';
 import { config } from './config';
 import { LoggingInterceptor } from './common/interceptors/logging/logging.interceptor';
 import { GlobalFilter } from './common/filters/global/global.filter';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, () => {
+    logger.log(`Nest application listening on port: ${config.PORT}`);
+  });
 
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalFilters(new GlobalFilter());
